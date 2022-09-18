c4a8ac0efdf0f48dcb0ff82095a6998ded3c5975
jmeegan2 <jmeegan8@gmail.com>
2022-09-17 21:24:48 -0400

    passing data to components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-
-function App() {
-  const now = new Date()
-  const a = 10
-  const b = 20
+// Projects created with creat-react-app are configured to compile automatically.
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old 
+      </p>
+    </div>
+  )
+}
+//example of passing an argument into another function, callback 
+const App = () => {
+  const name = 'Peter'
+  const age = 10
 
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   )
 }
